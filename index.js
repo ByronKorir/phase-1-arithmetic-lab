392a2a9a701cddf333ed71744d24f91051ac28b1
byron korir <byron.korir@student.moringaschool.com>
2023-09-19 15:12:52 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1*num2;
+
+const random = 10;
+Math.random();
+
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3%num4;
+
+const max= 20;
+
+
+
+
